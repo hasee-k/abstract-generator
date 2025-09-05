e4660cf915bf6f8eacf0e11de585f0f3ecf28787
Hasee <haseekumanayake@gmail.com>
2025-09-05 22:06:23 +0530

    Llama implementation

--- LlamaImplementation.py
+++ LlamaImplementation.py
@@ -24,22 +24,29 @@ def extract_important_points(text):
    completion = client.chat.completions.create(
             model="meta-llama/Meta-Llama-3-8B-Instruct",
             messages=[
+                {"role": "system", "content": "You are a helpful assistant that helps extract important points from a research paper. "
+                                              "These points will be then used to generate the abstract of the paper."},
                 {"role": "user", "content": prompt}
             ],
-            max_tokens=150,
+            max_tokens=600,
             temperature=0.7
         )
    return completion.choices[0].message.content
 
 
 def summarize_text(results):
-    prompt = f"Summarize the following text:\n{results}"
+    prompt = (f"I will provide you several points extracted from a research paper. Based on the following points, i want you to generate an abstract for this paper."
+              f"GIVE THE ABSTRACT IN PLAIN TEXT"
+              f"****** Extracted Points***** \n{results}"
+              f"Give only the abstract as the output."
+              f"REMINDER:Give the output in plain text without any numbering or bullet points.")
     completion = client.chat.completions.create(
         model="meta-llama/Meta-Llama-3-8B-Instruct",
         messages=[
+            {"role": "system", "content": "You are a helpful assistant that helps write the abstract a standard research paper.An abstract is a concise, standalone summary of a research paper that highlights its purpose, methods, key findings, and conclusions, allowing readers to understand the study's significance without reading the full text. It should be a single, coherent paragraph, usually 150-300 words, written in plain language without jargon, and should not include references, citations, or information not present in the main paper."},
             {"role": "user", "content": prompt}
         ],
-        max_tokens=150,
-        temperature=0.7
+        max_tokens=300,
+        temperature=0.4
     )
     return completion.choices[0].message.content
